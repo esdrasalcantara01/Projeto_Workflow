401387363ef6e1a2aded4ced9c3417e9a8205088
Esdras Alcântara <esdrascristao.4@gmail.com>
2026-09-02 11:48:27 -0300

    'aplicando protocolos de segurança'

--- WorkFlow/__init__.py
+++ WorkFlow/__init__.py
@@ -4,10 +4,13 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'chave-secreta-do-workflow'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:@localhost/WorkFlow'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
